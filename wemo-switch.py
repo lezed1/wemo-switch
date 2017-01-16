4dd62c7b88255ab64d0e43f5c2c3fc47f3441033
Zander Bolgar <lezed1@gmail.com>
2017-01-16 07:55:24 -0500

    GPIO control

---
+++ wemo-switch.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+
+
+## Configure these VV ##
+
+switch_pin = 27
+led_pin = 4
+
+## Configure these ^^ ##
+
+
+state = False
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(led_pin, GPIO.OUT)
+
+def switch_handler(channel):
+    state = not state
+    GPIO.output(led_pin, state)
+    print("Got button press! (on channel {})".format(channel))
+
+
+GPIO.add_event_detect(switch_pin, GPIO.RISING, callback=switch_handler, bouncetime=750)
+
+input("Press enter to end")
+
+GPIO.cleanup()
